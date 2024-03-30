907c4e137279bbeb3f02baed305d279804c9a7e4
Nicolas Yanase <nicolasyanase18@gmail.com>
2024-03-30 19:08:56 -0300

    projeto finalizado

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-facebook": "#1877f2",
+        "gray-facebook": "#f0f2f5",
+        "green-facebook": "#36A420",
+      },
+      spacing: {
+        530: "580px",
+        380: "380px",
+      },
+    },
+  },
+  plugins: [],
+};
